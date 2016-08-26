538631e268f1d98292950246c926b92b5b6546a0
huulam <huulam.nguyen@ved.com.vn>
2016-08-26 11:34:12 +0700

    lam

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('backend.authenticate');
+        $this->middleware('backend.authenticate');
         //$this->middleware('backend.permission');
     }
 
