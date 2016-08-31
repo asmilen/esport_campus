4b19bd454bba0e2ec6f4f1c6660762b93e8838c7
Nam vu <hongnam.vu@ved.com.vn>
2016-08-31 10:05:16 +0700

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\University;
+use App\Question;
 use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
@@ -47,6 +48,34 @@ class DatabaseSeeder extends Seeder
             'image' => '',
             'status' => 1,
         ]);
+
+
+        Question::truncate();
+        for ($i=1; $i < 200; $i++) { 
+            DB::table('questions')->insert([
+                'question' => md5(random_int(1, 1000)),
+                'type'     => 0,
+                'level'    => random_int(1,2),
+                'category' => random_int(1,4)
+                ]);
+            for ($j=0; $j < 4; $j++) { 
+                DB::table('answers')->insert([
+                'question_id' => $i,
+                'answer' => md5(random_int(1, 1000)),
+                'status'    => ($j == 0) ? 1:0
+                ]);
+            }
+        }
+
+        for ($i=0; $i < 20; $i++) { 
+            DB::table('questions')->insert([
+                'question' => md5(random_int(1, 1000)),
+                'type'     => 1,
+                'level'    => random_int(1,2),
+                'category' => random_int(1,2)
+                ]);
+        }
+
         Model::reguard();
         DB::statement("SET foreign_key_checks=1");
     }
